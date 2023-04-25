b57d06c0c1dbdb674e6e69e3a740daefea822bf0
ziyixiee <ziyixie@andrew.cmu.edu>
2023-04-25 21:07:16 +0000

    used black and pylint to format the code

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='s23openalex',
-    version='0.1',
-    packages=['s23openalex'],
-    install_requires=['bibtexparser'],
+    name="s23openalex",
+    version="0.1",
+    packages=["s23openalex"],
+    install_requires=["bibtexparser"],
 )
